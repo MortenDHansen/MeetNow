f1e562a83f9426bc16e8a9117c9cafa85980d37a
Morten D. Hansen <morten@visia.dk>
2016-12-03 13:44:01 +0100

    Added an alert. Yay, i rock

--- MeetNow/src/app/app.component.ts
+++ MeetNow/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import {AlertController} from "ionic-angular";
 
 
 @Component({
@@ -11,12 +12,42 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage = HomePage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, private alertCtrl: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.makeAlert();
       StatusBar.styleDefault();
       Splashscreen.hide();
+
+
+
+    }
+
+    );
+
+  }
+
+  public makeAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Nearby Match',
+      message: 'Do you want to meet?',
+      buttons: [
+        {
+          text: 'No, thank you',
+          role: 'cancel',
+          handler: () => {
+            console.log('Callback NO');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('Callback Yes');
+          }
+        }
+      ]
     });
+    alert.present();
   }
 }
